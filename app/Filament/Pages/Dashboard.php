8e02cf814ada3965951a2970807f6fe29a812a09
DiogenesTSO <diogenestobias@hotmail.com>
2025-07-11 10:51:01 -0300

    feat: :sparkles: Nova dashboard
    Nova dashboard
    
    Nova dashboard

--- app/Filament/Pages/Dashboard.php
+++ app/Filament/Pages/Dashboard.php
@@ -2,9 +2,11 @@
 
 namespace App\Filament\Pages;
 
+use App\Filament\Widgets\BoasVindas;
 use App\Filament\Widgets\FinanceiroStats;
 use App\Filament\Widgets\GraficoDespesas;
 use App\Filament\Widgets\GraficoParcelas;
+use App\Models\Transacao;
 use Filament\Pages\Dashboard as BaseDashboard;
 
 class Dashboard extends BaseDashboard
@@ -14,6 +16,14 @@ class Dashboard extends BaseDashboard
 
     public function getWidgets(): array
     {
+        $temDados = Transacao::where('familia_id', filament()->auth()->user()->familia_id)->exists();
+
+        if(!$temDados) {
+            return [
+                BoasVindas::class,
+            ];
+        }
+
         return [
             FinanceiroStats::class,
             GraficoDespesas::class,
