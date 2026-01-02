301424ce9c7f01970b61e2cbe9558dcde0bc96dd
DiogenesTSO <diogenestobias@hotmail.com>
2026-01-02 16:49:46 -0300

    [FEAT]: Implementado função para atualizar o saldo atual quando criar uma transação

--- app/Filament/Resources/TransacoesResource/Pages/CreateTransacoes.php
+++ app/Filament/Resources/TransacoesResource/Pages/CreateTransacoes.php
@@ -3,10 +3,32 @@
 namespace App\Filament\Resources\TransacoesResource\Pages;
 
 use App\Filament\Resources\TransacoesResource;
+use App\Models\Conta;
 use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateTransacoes extends CreateRecord
 {
     protected static string $resource = TransacoesResource::class;
+
+    protected function afterCreate(): void
+    {
+        $transacao = $this->record;
+
+        if (!$transacao->is_paid) {
+            return;
+        }
+
+        $conta = Conta::find($transacao->conta_id);
+
+        if (!$conta) {
+            return;
+        }
+
+        match ($transacao->tipo) {
+            'receita' => $conta->increment('saldo_atual', $transacao->valor),
+            'despesa' => $conta->decrement('saldo_atual', $transacao->valor),
+            default   => null,
+        };
+    }
 }
